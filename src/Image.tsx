984c7f4fb9c6758997ddac51f84c3c11ff9d3739
Alex Zyu <alexzyu@Alexs-MacBook-Pro.local>
2025-02-02 17:08:43 +0400

    created app, added router

---
+++ src/Image.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+export default function Image() {
+
+  return (
+    <>
+      <div>
+        Image P
+      </div>
+      <p className="read-the-docs">
+        <Link to='/'>LINK TO [MAIN]</Link>
+      </p>
+    </>
+  )
+}
+
